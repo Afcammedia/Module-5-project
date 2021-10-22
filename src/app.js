631395a64c6cf071acc99c55f58c6741bf38eeea
Jackson Achemu <achemuj@gmail.com>
2021-10-22 16:25:28 -0500

    completed the search and filter feature

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dataBaseConnection = require("./db/db.mongoDB");
-const router = require("./routes/router");
-const hbs = require("express-handlebars");
-const path = require("path");
+const dataBaseConnection = require('./db/db.mongoDB');
+const router = require('./routes/router');
+const hbs = require('express-handlebars');
+const path = require('path');
 
 dataBaseConnection();
 
@@ -12,56 +12,56 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Views
-app.use(express.static(__dirname + "/views/static"));
-app.set("views", path.join(__dirname, "views/partials"));
-app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/views/static'));
+app.set('views', path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
 app.engine(
-	"hbs",
-	hbs({
-		extname: "hbs",
-		defaultLayout: "layout.hbs",
-		layoutsDir: __dirname + "/views/layouts",
-		partialsDir: __dirname + "/views/partials",
-		helpers: {
-			courseDetails: function (aString, bString) {
-				return `/course-details/${aString}/${bString}`;
-			},
-			homePage: function (aString) {
-				return `/${aString}`;
-			},
-			sharecourse: function (sString) {
-				let result = "/create-course/" + sString;
-				return result;
-			},
-			courseSrc: function (sString) {
-				return `/courses-page/` + sString;
-			},
-			logOutSrc: function (sString) {
-				return `/logout/` + sString;
-			},
-			editcourse: function (id) {
-				return `/course/${id}`;
-			},
-			deleteCourse: function (id) {
-				return `deleteCourse('${id}')`;
-			},
-			likecourse: function (id) {
-				return `likecourse('${id}')`;
-			},
-			updateCourse: function (aString, bString) {
-				return `/update-course/${aString}/${bString}`;
-			},
-			enrollCourse: function (userId, courseId) {
-				const mString = userId + " , " + courseId;
-				return `enrollCourse(${mString})`;
-			},
-			ingredients: function (aString) {
-				return aString.reduce((word, index) => {
-					word + ` ${index}`;
-				}, "");
-			},
-		},
-	})
+    'hbs',
+    hbs({
+        extname: 'hbs',
+        defaultLayout: 'layout.hbs',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials',
+        helpers: {
+            courseDetails: function (aString, bString) {
+                return `/course-details/${aString}/${bString}`;
+            },
+            homePage: function (aString) {
+                return `/${aString}`;
+            },
+            sharecourse: function (sString) {
+                let result = '/create-course/' + sString;
+                return result;
+            },
+            courseSrc: function (sString) {
+                return `/courses-page/` + sString;
+            },
+            logOutSrc: function (sString) {
+                return `/logout/` + sString;
+            },
+            editcourse: function (id) {
+                return `/course/${id}`;
+            },
+            deleteCourse: function (id) {
+                return `deleteCourse('${id}')`;
+            },
+            likecourse: function (id) {
+                return `likecourse('${id}')`;
+            },
+            updateCourse: function (aString, bString) {
+                return `/update-course/${aString}/${bString}`;
+            },
+            enrollCourse: function (userId, courseId) {
+                const mString = userId + ' , ' + courseId;
+                return `enrollCourse(${mString})`;
+            },
+            ingredients: function (aString) {
+                return aString.reduce((word, index) => {
+                    word + ` ${index}`;
+                }, '');
+            },
+        },
+    })
 );
 
 /*
@@ -69,17 +69,17 @@ User route = '/api/v1/users'
 Courses route = '/api/v1/courses'
 Views route = '/'
 */
-app.use("/", router);
+app.use('/', router);
 // app.use('/api/v1/users');
 
 // Global Error Handling
 app.use((err, req, res, next) => {
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || "error";
-	return res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
 });
 
 module.exports = app;
--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,85 +1,85 @@
-const AppError = require("../utils/handle.errors");
-const authService = require("../services/auth.service");
-const courseService = require("../services/course.service");
+const AppError = require('../utils/handle.errors');
+const authService = require('../services/auth.service');
+const courseService = require('../services/course.service');
 
 class ViewsController {
-	constructor() {}
+    constructor() {}
 
-	async courseDetails(req, res) {
-		const id = req.params.id;
-		const courseId = req.params.courseId;
-		const user = await authService.findUser(id);
-		const course = await courseService.findCourse(courseId);
-		let isTrue = false;
-		let isEnrolled = false;
-		if (id === courseId) isTrue = true;
-		res.render("course-details", {
-			layout: "layout",
-			data: user,
-			course: course.course,
-			isTrue: isTrue,
-			isEnrolled: isEnrolled,
-		});
-	}
+    async courseDetails(req, res) {
+        const id = req.params.id;
+        const courseId = req.params.courseId;
+        const user = await authService.findUser(id);
+        const course = await courseService.findCourse(courseId);
+        let isTrue = true;
+        let isEnrolled = false;
+        if (id === courseId) isTrue = true;
+        res.render('course-details', {
+            layout: 'layout',
+            data: user,
+            course: course.course,
+            isTrue: isTrue,
+            isEnrolled: isEnrolled,
+        });
+    }
 
-	async homePage(req, res) {
-		const id = req.params.id;
-		const courses = await courseService.findAllCourse();
-		const course = courses.course.map((item) => {
-			return { ...item, userId: id };
-		});
-		if (id) {
-			const user = await authService.findUser(id);
-			return res.render("home", {
-				layout: "layout",
-				course: course,
-				user: user,
-			});
-		}
-		return res.render("home", {
-			layout: "layout",
-			course: course,
-		});
-	}
+    async homePage(req, res) {
+        const id = req.params.id;
+        const courses = await courseService.findAllCourse();
+        const course = courses.course.map((item) => {
+            return { ...item, userId: id };
+        });
+        if (id) {
+            const user = await authService.findUser(id);
+            return res.render('home', {
+                layout: 'layout',
+                course: course,
+                user: user,
+            });
+        }
+        return res.render('home', {
+            layout: 'layout',
+            course: course,
+        });
+    }
 
-	async login(req, res) {
-		res.render("login", {
-			layout: "layout",
-		});
-	}
-	async signUp(req, res) {
-		res.render("signUp", {
-			layout: "layout",
-		});
-	}
+    async login(req, res) {
+        res.render('login', {
+            layout: 'layout',
+        });
+    }
+    async signUp(req, res) {
+        res.render('signUp', {
+            layout: 'layout',
+        });
+    }
 
-	async createCourse(req, res) {
-		const id = req.params.id;
-		const user = await authService.findUser(id);
-		res.render("create-course", {
-			layout: "layout",
-			username: user.username,
-			id: user.id,
-		});
-	}
+    async createCourse(req, res) {
+        const id = req.params.id;
+        const user = await authService.findUser(id);
+        res.render('create-course', {
+            layout: 'layout',
+            username: user.username,
+            id: user.id,
+        });
+    }
 
-	async editCourse(req, res, next) {
-		const id = req.params.userId;
-		const courseId = req.params.courseId;
-		const user = await authService.findUser(id);
-		const course = await courseService.findCourse(courseId);
-		res.render("update-course", {
-			layout: "layout",
-			user: user,
-			course: course.course,
-		});
-	}
+    async editCourse(req, res, next) {
+        const id = req.params.userId;
+        const courseId = req.params.courseId;
+        const user = await authService.findUser(id);
+        const course = await courseService.findCourse(courseId);
+        res.render('update-course', {
+            layout: 'layout',
+            user: user,
+            course: course.course,
+        });
+    }
 
-	async logOut(req, res) {
-		const id = req.params.id;
+    async logOut(req, res) {
+        const id = req.params.id;
 
-		res.redirect("/");
-	}
+        res.redirect('/');
+    }
 }
 
 module.exports = new ViewsController();
