e08db0591e87699d5d281fe9f87143cac5e55102
Jackson Achemu <achemuj@gmail.com>
2021-10-26 04:42:35 -0500

    completed user login and logout, completed home page search feature and enroll feature

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,94 +1,91 @@
-const authService = require('../services/auth.service');
-const courseService = require('../services/course.service');
+const authService = require("../services/auth.service");
+const courseService = require("../services/course.service");
 
 class ViewsController {
-    constructor() {}
+	constructor() {}
 
-    async courseDetails(req, res) {
-        const id = req.params.userId;
-        const courseId = req.params.courseId;
-        const user = await authService.findUser(id);
-        const courseRes = await courseService.findCourse(courseId);
-        const { course } = courseRes;
-        let isEnrolled = course?.enrolledUsers.some((item) => {
-            return item.username === user.username;
-        });
-        console.log(id, 'd');
-        let creator = false;
-        if (id != course?.createdBy._id) {
-            creator = true;
-            console.log(creator, 'the creator');
-        }
-        res.render('course-details', {
-            // layout: 'layout',
-            data: user,
-            course: courseRes.course,
-            creator: creator,
-            isEnrolled: isEnrolled,
-        });
-    }
+	async courseDetails(req, res) {
+		const id = req.params.userId;
+		const courseId = req.params.courseId;
+		const user = await authService.findUser(id);
+		const courseRes = await courseService.findCourse(courseId);
+		const { course } = courseRes;
+		let isEnrolled = course.enrolledUsers.some((item) => {
+			return item.username === user.username;
+		});
+		let creator = false;
+		if (id === course.createdBy._id.toString()) {
+			creator = true;
+		}
+		res.render("course-details", {
+			data: user,
+			course: courseRes.course,
+			creator: creator,
+			isEnrolled: isEnrolled,
+		});
+	}
 
-    async homePage(req, res) {
-        const id = req.params.id;
-        const courses = await courseService.findAllCourse();
-        const user = await authService.findUser(id);
-        const course = courses.course.map((item) => {
-            return { ...item, userId: id };
-        });
-        if (!id) {
-            return res.render('home', {
-                layout: 'layout',
-                course: course,
-            });
-        }
-        return res.render('home', {
-            layout: 'layout',
-            course: course,
-            user: user,
-        });
-    }
+	async homePage(req, res) {
+		const id = req.params.id;
+		const courses = await courseService.findAllCourse();
+		const user = await authService.findUser(id);
+		const course = courses.course.map((item) => {
+			return { ...item, userId: id };
+		});
+		if (!id) {
+			return res.render("home", {
+				layout: "layout",
+				course: course,
+			});
+		}
+		return res.render("home", {
+			layout: "layout",
+			course: course,
+			user: user,
+		});
+	}
 
-    async login(req, res) {
-        res.render('login', {
-            layout: 'layout',
-        });
-    }
+	async login(req, res) {
+		res.render("login", {
+			layout: "layout",
+		});
+	}
 
-    async signUp(req, res) {
-        res.render('signUp', {
-            layout: 'layout',
-        });
-    }
+	async signUp(req, res) {
+		res.render("signUp", {
+			// layout: 'layout',
+		});
+	}
 
-    async createCourse(req, res) {
-        const id = req.params.id;
-        const user = await authService.findUser(id);
-        res.render('create-course', {
-            layout: 'layout',
-            username: user.username,
-            id: user.id,
-        });
-    }
+	async createCourse(req, res) {
+		const id = req.params.id;
+		const user = await authService.findUser(id);
+		res.render("create-course", {
+			layout: "layout",
+			username: user.username,
+			id: user.id,
+		});
+	}
 
-    async editCourse(req, res, next) {
-        const id = req.params.userId;
-        const courseId = req.params.courseId;
-        const user = await authService.findUser(id);
-        const course = await courseService.findCourse(courseId);
-        res.render('update-course', {
-            layout: 'layout',
-            user: user,
-            course: course.course,
-        });
-    }
+	async editCourse(req, res, next) {
+		const id = req.params.userId;
+		const courseId = req.params.courseId;
+		const user = await authService.findUser(id);
+		const course = await courseService.findCourse(courseId);
+		res.render("update-course", {
+			layout: "layout",
+			user: user,
+			course: course.course,
+		});
+	}
 
-    async logOut(req, res) {
-        res.cookie('Token', '', {
-            httpOnly: true,
-            maxAge: 1000,
-        });
-        res.redirect('/');
-    }
+	async logOut(req, res) {
+		res.cookie("Token", "", {
+			httpOnly: true,
+			maxAge: 1000,
+		});
+		res.redirect("/");
+	}
 }
 
 module.exports = new ViewsController();
--- src/views/static/logic/login.js
+++ src/views/static/logic/login.js
@@ -10,7 +10,6 @@ submit.addEventListener('click', async (e) => {
         username: username.value,
         password: password.value,
     };
-    console.log(formData);
     try {
         const result = await axios.post('/api/v1/users/login', formData);
         const { data } = result;
@@ -56,4 +55,37 @@ const fetchData = (username, password) => {
         username: username,
         password: password,
     };
+};
+
+const successBox = document.getElementById('successBox');
+const loadingBox = document.getElementById('loadingBox');
+const errorBox = document.getElementById('errorBox');
+const deleteCourse = async (id, userId) => {
+    try {
+        await axios.delete(`/api/v1/courses/${id}`);
+        window.location.replace(`/home/${userId}`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+const enrollCourse = async (userId, courseId) => {
+    try {
+        const result = await axios.put(`/api/v1/courses/enroll`, {
+            userId: userId,
+            courseId: courseId,
+        });
+        alertMessage(successBox, 'success-message', 'Enrolled');
+        location.reload();
+    } catch (err) {
+        console.log(err);
+    }
+};
+const alertMessage = (element, messageClass, message) => {
+    element.classList.toggle('visible');
+    const innerMessage = document.querySelector(`.${messageClass}`);
+    innerMessage.innerHTML = message;
+    timeout = setTimeout(() => {
+        element.classList.toggle('visible');
+    }, 5000);
+    return timeout;
 };
